0a8308136e8bd7da6ca62431e003c6c7054be21c
jeyson velasquez <jeysonvelasquez.v@gmail.com>
2022-04-27 17:45:46 -0500

    test clase Usuario

--- src/main/java/test/TestManejoObjects.java
+++ src/main/java/test/TestManejoObjects.java
@@ -1,13 +1,16 @@
 package test;
 
 import datos.PersonaDAO;
+import datos.UsuarioDAO;
 import domain.Persona;
+import domain.Usuario;
 import java.util.*;
 
-public class TestManejoPersonas {
+public class TestManejoObjects {
 
     public static void main(String[] args) {
 
+// Objeto de Tipo Persona
         //Se crea o instancia un objeto de la Clase PersonaDAO
 //        PersonaDAO personaDAO = new PersonaDAO();
         //Se llama al metodo listarDatos y se guarda la informacion en una lista de tipo Persona
@@ -35,11 +38,35 @@ public class TestManejoPersonas {
 //            System.out.println("persona = " + persona);
 //
 //        }
+//Objeto de tipo Usuario
+//Se crea o instancia un objeto de la Clase UsuarioDAO
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        //Se llama al metodo listarDatos y se guarda la informacion en una lista de tipo Usuario
+//        List<Usuario> usuarios = usuarioDAO.listarDatos();
+//////
+//////        //forEach normal
+////////        for (Persona persona : personas) {
+////////            
+////////            System.out.println("persona = " + persona);
+////////
+////////        }
+//        //Funcion Lamda
+//        usuarios.forEach(usuario -> {
+//            System.out.println("usuario = " + usuario);
+//        });
 
-            
+        //Se crea un objeto de tipo persona
+        Usuario user = new Usuario("jeyson", "Lafacil88");
 
+        usuarioDAO.insertarDatos(user);
 
+        List<Usuario> usuarios = usuarioDAO.listarDatos();
 
+        for (Usuario usuario : usuarios) {
+
+            System.out.println("Usuario = " + usuario);
+
+        }
 
     }
 
