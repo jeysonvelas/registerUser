95a46fb93c8afede634e8e71fc664e787f5f2c19
jeyson velasquez <jeysonvelasquez.v@gmail.com>
2022-04-27 17:29:18 -0500

    clase Usuario con su respectivo DAO

--- src/main/java/datos/UsuarioDAO.java
+++ src/main/java/datos/UsuarioDAO.java
@@ -1,13 +1,151 @@
-
 package datos;
 
+import domain.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
-    
-    
-    
-    
-    
-    
-    
+
+    private static final String SQL_SELECT = "SELECT * FROM usuario";
+    private static final String SQL_INSERT = "INSERT INTO usuario (usuario, password) VALUES (?, ?)";
+    private static final String SQL_UPDATE = "UPDATE persona SET usuario = ?, password = ? WHERE id_usuario = ? ";
+    private static final String SQL_DELETE = "DELETE from usuario WHERE id_usuario = ? ";
+
+    //Metodo para Listar registros de una Tabla
+    public List<Usuario> listarDatos() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Usuario user = null;
+        List<Usuario> usuarios = new ArrayList<>();
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+
+                int idUsuario = rs.getInt("id_usuario");
+                String usuario = rs.getString("usuario");
+                String password = rs.getString("password");
+
+                user = new Usuario(idUsuario, usuario, password);
+
+                usuarios.add(user);
+
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexion.close(rs);
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+
+        return usuarios;
+    }
+
+    //Metodo para insertar regitros en la Base de Datos
+    public int insertarDatos(Usuario usuario) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getPassword());
+
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+
+            try {
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+
+        }
+
+        return registros;
+
+    }
+
+    public int modificarDatos(Usuario usuario) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getPassword());
+            stmt.setInt(3, usuario.getIdUsuario());
+
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+
+            try {
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+
+        }
+
+        return registros;
+
+    }
+
+    public int eliminarDatos(Usuario usuario) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);
+
+            stmt.setInt(1, usuario.getIdUsuario());
+
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+
+            try {
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+
+        }
+
+        return registros;
+
+    }
+
 }
